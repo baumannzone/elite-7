ae403368578ac4fff977849dcf971d83ec2724ea
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2021-04-21 20:49:13 +0200

    chore: update

--- components/home/HeaderSection.vue
+++ components/home/HeaderSection.vue
@@ -1,25 +1,23 @@
 <template>
   <section class="section-header bg-gray-50">
     <div class="container">
-      <div>
-        <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div class="text-center">
-            <h2
-              class="text-base font-semibold text-red-600 tracking-wide uppercase"
-            >
-              Elite 7
-            </h2>
-            <p
-              class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"
-            >
-              Consultores y asesores
-            </p>
-            <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-              Somos una empresa formada por profesionales con más de 30 años de
-              experiencia. Consultoría y asesoramiento fiscal, contable,
-              jurídico y laboral para particulares o empresas.
-            </p>
-          </div>
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div class="text-center">
+          <h2
+            class="text-base font-semibold text-red-600 tracking-wide uppercase"
+          >
+            Elite 7
+          </h2>
+          <p
+            class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl"
+          >
+            Consultores y asesores
+          </p>
+          <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+            Somos una empresa formada por profesionales con más de 30 años de
+            experiencia. Consultoría y asesoramiento fiscal, contable, jurídico
+            y laboral para particulares o empresas.
+          </p>
         </div>
       </div>
     </div>
